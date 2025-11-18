92f3e0561985fb7f102454879496b8c3ab885371
Youfang yao <yyfsingler@163.com>
2025-11-18 11:36:47 -0700

    add BottomNavigationBar for home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int _currentIndex = 0;
+
     return Scaffold(
       appBar: AppBar(title: Text('Message')),
       body: Center(child: Text("Login user: ${loginUser.displayName}")),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _currentIndex = index;
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people, color: Colors.green),
+            label: "Contact",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, color: Colors.green),
+            label: "Messages",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.green),
+            label: "Me",
+          ),
+        ],
+      ),
     );
   }
 }
